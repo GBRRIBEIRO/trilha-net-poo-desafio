012c483db53db5cc0d2d87175ab9b3dc0ac73920
Gabriel Nunes Ribeiro <gabrieldexribeiro@hotmail.com>
2023-11-20 17:41:36 -0300

    Done

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123", "Tijolão", "000", 200);
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("Youtube");
+
+Iphone iphone = new Iphone("124", "13", "001", 100);
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("Youtube");
